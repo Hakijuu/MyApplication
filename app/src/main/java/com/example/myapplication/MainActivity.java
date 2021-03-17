f505f0cff527bb9ceb5bfa58edbc4a658ff542f1
Hakijuu <chwil.malgorzata99@gmail.com>
2021-03-17 11:04:35 +0100

    First app

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,192 @@
+package com.example.myapplication;
+
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    boolean nameChecked = false, lastnameChecked = false, numberChecked = false;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //Tworzenie komponentów, odczytywanie referencji do elementów GUI (findViewById()) ustawienie obsługi zdarzeń, odczytywanie zapisanego stanu aktywności z obiektu savedInstanceState
+        setContentView(R.layout.activity_main);
+
+        Button button = findViewById(R.id.button);
+        button.setVisibility(View.INVISIBLE);
+
+        TextView name = findViewById(R.id.poleImie);
+        TextView lastname = findViewById(R.id.poleNazwisko);
+        TextView number= findViewById(R.id.poleLiczba);
+
+
+
+        name.setOnFocusChangeListener((v, hasFocus) -> {
+            if(!hasFocus) {
+                if(!checkName(name.getText().toString())) {
+                    name.setError(getString(R.string.imie_EditText));
+                    Toast.makeText(MainActivity.this,R.string.imie_EditText,Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        lastname.setOnFocusChangeListener((v, hasFocus) -> {
+            if(!hasFocus) {
+                if(!checkLastname(lastname.getText().toString())) {
+                    lastname.setError(getString(R.string.nazwisko_EditText));
+                    Toast.makeText(MainActivity.this,R.string.nazwisko_EditText,Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        number.setOnFocusChangeListener((v, hasFocus) -> {
+            if(!hasFocus) {
+                if(!checkNumber(number.getText().toString())) {
+                    number.setError(getString(R.string.liczba_ocen_EditText));
+                    Toast.makeText(MainActivity.this,R.string.liczba_ocen_EditText,Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+
+        name.addTextChangedListener(new TextWatcher(){
+
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if(s.length()<=0){
+                    nameChecked=false;
+                    button.setVisibility(View.INVISIBLE);
+                }
+                else{
+                    nameChecked=true;
+                    if(lastnameChecked && numberChecked){
+                        button.setVisibility(View.VISIBLE);
+                    }
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {}
+        });
+
+        lastname.addTextChangedListener(new TextWatcher(){
+
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if(s.length()<=0){
+                    lastnameChecked=false;
+                    button.setVisibility(View.INVISIBLE);
+                }
+                else{
+                    lastnameChecked=true;
+                    if(nameChecked && numberChecked){
+                        button.setVisibility(View.VISIBLE);
+                    }
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {}
+        });
+
+        number.addTextChangedListener(new TextWatcher(){
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) { }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                int liczba1=0;
+                try {
+                    liczba1 = Integer.parseInt(s.toString());
+                } catch(NumberFormatException e) {
+                    System.out.println("Could not parse " + e);
+                }
+                if(liczba1>15 || liczba1 <5){
+                    numberChecked=false;
+                    button.setVisibility(View.INVISIBLE);
+                }
+                else{
+                    numberChecked=true;
+                    if(nameChecked && lastnameChecked){
+                        button.setVisibility(View.VISIBLE);
+                    }
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {}
+        });
+    }
+
+    public boolean checkName(String nameS){
+        return nameS.length() != 0;
+    }
+
+    public boolean checkLastname(String lastnameS){
+        return lastnameS.length() != 0;
+    }
+
+    public boolean checkNumber(String liczbaS){
+        try {
+            int liczba = Integer.parseInt(liczbaS);
+            return liczba >= 5 && liczba <= 15;
+        } catch (NumberFormatException e) {
+            System.out.println("Could not parse " + e);
+            return false;
+        }
+    }
+
+
+    //Aktywność była niewidoczna
+    protected void onRestart(){
+        super.onRestart();
+        //Czynności wykonywane gdy aktywność wcześniej istniała ale była niewidoczna
+    }
+
+    //Aktywność za chwilę stanie się widoczna
+    @Override
+    protected void onStart() {
+        super.onStart();
+        //Tworzenie elementów niezbędnych do uaktualniania interfejsu użytkownika
+    }
+
+    //Aktywność jest na pierwszym planie
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    //aktywność traci "focus"/ Zostanie zapauzowana (gdy system ma wznowić inną aktywność).
+    @Override
+    protected void onPause() {
+        super.onPause();
+        //Tutaj należy zwolnić zasoby i zapisać istotne dane w trwałym magazynie. Implemetacja powinna być szybka bo system nie wznowi innej aktywnośći dopóki ta metoda się nie zakończy
+    }
+
+    //Aktywność nie jest widoczna. Została wstrzymana.
+    @Override
+    protected void onStop() {
+        super.onStop();
+        //Tutaj należy zwolnić zasoby i ew. zapisać istotne elementy stanu. Po wykonaniu tej metody proces z tą aktywnością może zostać "zabity" przez system (bezy wykonywania kolejnych metod cyklu życia).
+    }
+
+    //Za chwiolę aktywność zostanie zniszczona (tymczasowo lub w celu zwolnienia pamięci)
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        //po wykonaniu tej metody proces z tą aktywnością może zostać "zabity" przez system (bez wykonania kolejnych metod cyklu życia).
+    }
+}
